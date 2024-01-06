c10cdd0bb32294eaa621f6cb5ea89f4bdf46a05f
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 02:56:32 +0900

    e

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -112,6 +112,7 @@ return {
     },
     config = function()
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
 
       cmp.setup({
         snippet = {
@@ -135,7 +136,23 @@ return {
           { name = "vsnip" },
         }, {
           { name = "buffer" },
-        })
+          { name = "calc" },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                           -- can also be a function to dynamically calculate max width such as
+                           -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+              return vim_item
+            end
+          })
+        }
       })
 
       -- Set configuration for specific filetype.
