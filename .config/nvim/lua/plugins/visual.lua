107af2cf952e58f8a866b6f569d3cd8e2c604848
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 01:31:16 +0900

    e

--- .config/nvim/lua/plugins/visual.lua
+++ .config/nvim/lua/plugins/visual.lua
@@ -45,13 +45,22 @@ return {
     build = ":TSUpdate",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
+    opts = {
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "typescript",
+        "html"
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
   }
 }
