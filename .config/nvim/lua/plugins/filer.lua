b841b79419ce4e2508e126c1975db5b22fee7cf8
fumimaron9 <fumimaron9@gmail.com>
2024-01-06 18:08:28 +0900

    e

--- .config/nvim/lua/plugins/filer.lua
+++ .config/nvim/lua/plugins/filer.lua
@@ -11,5 +11,23 @@ return {
     config = function()
       require("nvim-tree").setup()
     end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    cmd = {
+      "TSInstall",
+      "TSBufEnable",
+      "TSBufDisable",
+      "TSModuleInfo"
+    },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html" },
+        sync_install = false,
+        highlight = { enable = false },
+        indent = { enable = true },
+      })
+    end
   }
 }
