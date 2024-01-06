884bce3c2aa93881264bd37c20d8a736d006c350
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 02:32:46 +0900

    e

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,9 +15,8 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = {
-      { "williamboman/mason.nvim" },
-      { "neovim/nvim-lspconfig" },
-      { "echasnovski/mini.completion", version = false },
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig"
     },
     cmd = {
       "LspInstall",
@@ -26,7 +25,6 @@ return {
     event = "User FileOpened",
     config = function()
       local lspconfig = require("lspconfig")
-      require("mini.completion").setup()
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           lspconfig[server_name].setup(opts)
@@ -89,6 +87,90 @@ return {
           "spell", -- spell checker
         }
       })
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
+      { "hrsh7th/cmp-buffer", event = "InsertEnter" },
+      { "hrsh7th/cmp-path", event = "InsertEnter" },
+      { "hrsh7th/cmp-vsnip", event = "InsertEnter" },
+      { "hrsh7th/cmp-cmdline", event = "ModeChanged" },
+
+      { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
+      { "hrsh7th/cmp-nvim-lsp-document-symbol", event = "InsertEnter" },
+      { "hrsh7th/cmp-calc", event = "InsertEnter" },
+      { "onsails/lspkind.nvim", event = "InsertEnter" },
+      { "hrsh7th/vim-vsnip", event = "InsertEnter" },
+      { "hrsh7th/vim-vsnip-integ", event = "InsertEnter" },
+      { "rafamadriz/friendly-snippets", event = "InsertEnter" },
+    },
+    event = {
+      "InsertEnter",
+      "CmdlineEnter"
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          end
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+
+      -- Set configuration for specific filetype.
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+          { name = "buffer" },
+        })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          { name = "cmdline" }
+        })
+      })
+
+      -- Set up lspconfig.
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      require("lspconfig")["vtsls"].setup {
+        capabilities = capabilities
+      }
     end
   }
 }
