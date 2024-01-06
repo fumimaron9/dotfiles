31a9cca030fa23f05d2de05c79737f18637d0e26
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 01:20:58 +0900

    e

--- .config/nvim/lua/plugins/visual.lua
+++ .config/nvim/lua/plugins/visual.lua
@@ -19,9 +19,7 @@ return {
     },
     lazy = false,
     priority = 1000,
-    config = function()
-      require("bufferline").setup()
-    end
+    opts = {}
   },
   {
     "nvim-lualine/lualine.nvim",
