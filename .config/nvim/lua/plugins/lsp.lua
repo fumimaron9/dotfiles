59a966fed8db5756e135a9df3412194ffce7c7d7
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 12:22:59 +0900

    e

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -60,7 +60,11 @@ return {
       vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         vim.lsp.diagnostic.on_publish_diagnostics,
         {
-          virtual_text = false
+          virtual_text = {
+            format = function(diagnostic)
+              return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+            end,
+          }
         }
       )
     end
