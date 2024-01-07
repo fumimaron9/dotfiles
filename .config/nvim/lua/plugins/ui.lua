fd81b10755e35db6df34e2f7d65e0b0872771165
fumimaron9 <fumimaron9@gmail.com>
2024-01-07 09:49:40 +0900

    e

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -51,20 +51,20 @@ return {
         },
       })
 
-      local function open_nvim_tree(data)
-        local real_file = vim.fn.filereadable(data.file) == 1
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function(data)
+          local real_file = vim.fn.filereadable(data.file) == 1
 
-        -- buffer is a [No Name]
-        local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+          -- buffer is a [No Name]
+          local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-        if real_file and not no_name then
-          return
+          if real_file and not no_name then
+            return
+          end
+
+          require("nvim-tree.api").tree.toggle({ focus = true, find_file = true, })
         end
-
-        require("nvim-tree.api").tree.toggle({ focus = true, find_file = true, })
-      end
-
-      vim.api.nvim_create_autocmd("VimEnter", { callback = open_nvim_tree })
+      })
     end
   }
 }
