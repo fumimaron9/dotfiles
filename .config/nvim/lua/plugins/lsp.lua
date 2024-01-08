4c4fd6ade6c4d1362148a1e6a013e68823bfdc5d
fumimaron9 <fumimaron9@gmail.com>
2024-01-08 19:46:39 +0900

    e

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -78,7 +78,11 @@ return {
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            on_attach = function(client)
+              -- TODO: color
+              client.server_capabilities.semanticTokensProvider = nil
+            end
           })
         end
       })
