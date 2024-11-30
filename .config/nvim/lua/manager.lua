dd1e33e5d8854b7f1f9b5966933821de039cbff0
fumimaron9 <fumimaron9@gmail.com>
2024-11-30 12:06:46 +0900

    e

--- .config/nvim/lua/manager.lua
+++ .config/nvim/lua/manager.lua
@@ -16,6 +16,36 @@ require("lazy").setup("plugins", {
     lazy = true
   },
   performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport"
+      }
+    },
     cache = {
       enabled = true
     }
