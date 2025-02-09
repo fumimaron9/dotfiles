f80cc77c26c59dfacd5e1e4df753c77e396b861b
fumimaron9 <fumimaron9@gmail.com>
2025-02-09 14:12:00 +0900

    e

---
+++ home/dot_config/nvim/lua/options.lua
@@ -0,0 +1,11 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.o.encoding = "utf-8"
+vim.opt.number = true
+vim.opt.clipboard = "unnamed"
