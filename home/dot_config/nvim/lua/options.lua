3e406b4d2f47776745e6e90d1855e5a256b3c3cf
fumimaron9 <fumimaron9@gmail.com>
2025-02-11 16:01:00 +0900

    e

--- home/dot_config/nvim/lua/options.lua
+++ home/dot_config/nvim/lua/options.lua
@@ -1,11 +1,20 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.termguicolors = true
+vim.o.encoding = "utf-8"
+vim.o.number = true
+vim.o.termguicolors = true
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.g.loaded_netrw = true
+vim.g.loaded_netrwPlugin = true
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
-vim.o.encoding = "utf-8"
-vim.opt.number = true
-vim.opt.clipboard = "unnamed"
