0134cee64f6c638966052d008518754fc516fcec
fumimaron9 <fumimaron9@gmail.com>
2025-02-28 22:21:35 +0900

    e

--- home/private_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/private_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 mise activate pwsh | Out-String | Invoke-Expression
 
-Invoke-Expression (&starship init powershell)
-
 Set-Alias vi nvim
 Set-Alias ll ls
 Set-Alias l ls
+
+Invoke-Expression (&starship init powershell)
